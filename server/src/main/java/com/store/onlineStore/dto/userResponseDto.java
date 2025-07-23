b0494adc8c33488f8e078a8506a096aa017ada24
yj2980 <gum0945@naver.com>
2025-07-23 19:48:35 +0900

    feat: 회원 정보 수정을 위한 DTO 추가

---
+++ server/src/main/java/com/store/onlineStore/dto/userResponseDto.java
@@ -0,0 +1,11 @@
+package com.store.onlineStore.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class userResponseDto {
+	private String email;
+	private String username;
+}
